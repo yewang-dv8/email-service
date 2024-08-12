e94339571ad822669f779d97421be919d36b2cec
Ye Wang <ye.wang@dv8energy.com>
2024-08-12 14:19:22 -0600

    update 'to' email

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -50,7 +50,7 @@ export async function POST(request: Request) {
   try {
     await transporter.sendMail({
       from: `"${name}" <${email}>`,
-      to: 'ye.wang@dv8energy.com, wang77289271@gmail.com',
+      to: 'ye.wang@dv8energy.com, info@dv8energy.com',
       subject: 'Message From POC Gateway Page',
       text: `New message from poc gateway contact form:\n\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\n\nMessage:\n${message}`,
     })
